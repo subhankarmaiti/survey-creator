61ac9a57426c70fcb0691b9d912e0e359289ff08
Subhankar Maiti <subhankarmaiti40@gmail.com>
2020-01-16 01:09:16 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
-app.get("/welcome", (request, response) => {
-  response.send({ hi: "Hello" });
-});
+
 app.get("/", (req, res) => {
-  res.send({ node: "V10.11.0" });
+  res.send({ hi: "there" });
 });
-app.listen(5000, () => console.log("server running at 5000 port"));
+app.listen(5000);
