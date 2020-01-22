de8091a5b9ae09937c59e0d862c40d50a8e6d02a
Subhankar Maiti <subhankarmaiti40@gmail.com>
2020-01-23 00:23:30 +0530

    client added

---
+++ client/build/precache-manifest.75725b4cd918c68908f516fccbb96b20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51efd2b5c4737a93c93a7ef29d68caf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a33f5d9334a206d6555a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "163dd15cca523a63d899",
+    "url": "/static/js/2.970da5d1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.970da5d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a33f5d9334a206d6555a",
+    "url": "/static/js/main.4882dda8.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
